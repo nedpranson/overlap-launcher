edaee9f2e38b6b118d69314520903b8a6db46881
Nedas Pranskunas <nedas.pranskunas@gmail.com>
2025-12-24 00:37:06 +0200

    Add tray window.

--- main.c
+++ main.c
@@ -1,35 +1,111 @@
-#include <stdio.h>
 #include <windows.h>
 
 __declspec(dllexport) void __overlap_ignore_proc(void) {}
+
+LRESULT CALLBACK WndProc(HWND hWnd, UINT uMsg, WPARAM wParam, LPARAM lParam) {
+  switch (uMsg) {
+    case WM_DESTROY:
+      PostQuitMessage(0);
+      break;
+  }
+
+  return DefWindowProcW(hWnd, uMsg, wParam, lParam);
+}
 
 int main(int argc, char* argv[]) {
   if (argc != 2) {
     return 1;
   }
 
-  HMODULE lib = LoadLibrary(argv[1]);
-  if (!lib) {
+  HMODULE libModule = LoadLibraryA(argv[1]);
+  if (!libModule) {
     return 1;
   }
 
-  FARPROC proc = GetProcAddress(lib, "__overlap_hook_proc");
+  FARPROC proc = GetProcAddress(libModule, "__overlap_hook_proc");
   if (!proc) {
-    FreeLibrary(lib);
+    FreeLibrary(libModule);
     return 1;
   }
 
-  HHOOK hook = SetWindowsHookEx(WH_CBT, (HOOKPROC)proc, lib, 0);
+  HINSTANCE hInstance = GetModuleHandleW(NULL);
+  if (!hInstance) {
+    FreeLibrary(libModule);
+    return 1;
+  }
+
+  WNDCLASSW wndClass = {0};
+  wndClass.lpfnWndProc = WndProc;
+  wndClass.hInstance = hInstance;
+  wndClass.lpszClassName = L"OverlapTrayClass";
+
+  if (!RegisterClassW(&wndClass)) {
+    FreeLibrary(libModule);
+    return 1;
+  };
+
+  HWND hWnd = CreateWindowW(
+      wndClass.lpszClassName,
+      L"Overlap Tray Window",
+      WS_OVERLAPPEDWINDOW,
+      CW_USEDEFAULT,
+      CW_USEDEFAULT,
+      640,
+      480,
+      NULL,
+      NULL,
+      hInstance,
+      NULL
+  );
+
+  if (!hWnd) {
+    FreeLibrary(libModule);
+    return 1;
+  }
+
+  HICON icon = LoadImageW(
+      NULL,
+      (LPWSTR)IDI_APPLICATION,
+      IMAGE_ICON,
+      16,
+      16,
+      LR_SHARED
+  );
+
+  if (!icon) {
+    FreeLibrary(libModule);
+    return 1;
+  }
+
+  NOTIFYICONDATAW data = {0};
+  data.cbSize = sizeof(data);
+  data.hWnd = hWnd;
+  data.uID = 1;
+  data.uFlags = NIF_ICON | NIF_TIP;
+  data.hIcon = icon;
+  lstrcpyW(data.szTip, L"Overlap");
+
+  if (!Shell_NotifyIconW(NIM_ADD, &data)) {
+    FreeLibrary(libModule);
+    return 1;
+  }
+
+  HHOOK hook = SetWindowsHookEx(WH_CBT, (HOOKPROC)proc, libModule, 0);
   if (!hook) {
-    FreeLibrary(lib);
+    Shell_NotifyIconW(NIM_DELETE, &data);
+    FreeLibrary(libModule);
     return 1;
   }
 
-  printf("press enter to exit...");
-  getchar();
+  MSG msg;
+  while (GetMessageW(&msg, NULL, 0, 0)) {
+    TranslateMessage(&msg);
+    DispatchMessageW(&msg);
+  }
 
+  Shell_NotifyIconW(NIM_DELETE, &data);
   UnhookWindowsHookEx(hook);
-  FreeLibrary(lib);
+  FreeLibrary(libModule);
 
   return 0;
 }
