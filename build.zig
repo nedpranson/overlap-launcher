c806c22d067deaca0002caac63a329a843016843
Nedas Pranskunas <nedas.pranskunas@gmail.com>
2026-01-31 19:11:57 +0200

    Render options page.

--- build.zig
+++ build.zig
@@ -1,7 +1,7 @@
 const std = @import("std");
 
 pub fn build(b: *std.Build) void {
-    const target = b.standardTargetOptions(.{});
+    const target = b.standardTargetOptions(.{ .default_target = .{ .os_tag = .windows } });
     const optimize = b.standardOptimizeOption(.{});
 
     const nuklear = b.dependency("nuklear", .{
@@ -20,17 +20,26 @@ pub fn build(b: *std.Build) void {
     });
 
     exe.linkLibC();
+    exe.linkSystemLibrary("user32");
     exe.linkSystemLibrary("d3d9");
 
-    const flags = &[_][]const u8 {};
+    const flags = &[_][]const u8 {
+        "-Wall",
+        "-Wextra",
+        "-Wpedantic",
+        //"-Werror",
+        "-std=c23",
+    };
 
     exe.addIncludePath(nuklear.path(""));
     exe.addIncludePath(nuklear.path("demo/d3d9"));
     
     exe.addCSourceFile(.{
-        .file = b.path("src/main.c"),
+        .file = b.path("main.c"),
         .flags = flags,
     });
     
+    exe.subsystem = .Windows;
+
     b.installArtifact(exe);
 }
--- main.c
+++ main.c
@@ -1,6 +1,22 @@
+#define COBJMACROS
 #define WIN32_LEAN_AND_MEAN
 #include <windows.h>
 #include <shellapi.h>
+
+#define NK_INCLUDE_FIXED_TYPES
+#define NK_INCLUDE_STANDARD_IO
+#define NK_INCLUDE_STANDARD_VARARGS
+#define NK_INCLUDE_DEFAULT_ALLOCATOR
+#define NK_INCLUDE_VERTEX_BUFFER_OUTPUT
+#define NK_INCLUDE_FONT_BAKING
+#define NK_INCLUDE_DEFAULT_FONT
+#define NK_IMPLEMENTATION
+#define NK_D3D9_IMPLEMENTATION
+#include "nuklear.h"
+#include "nuklear_d3d9.h"
+
+#define WINDOW_WIDTH 600
+#define WINDOW_HEIGHT 800
 
 #define WM_TRAYICON (WM_USER + 1)
 
@@ -9,7 +25,8 @@
 
 __declspec(dllexport) void __overlap_ignore_proc(void) {}
 
-LRESULT CALLBACK WndProc(HWND hWnd, UINT uMsg, WPARAM wParam, LPARAM lParam) {
+static LRESULT CALLBACK
+WndProc(HWND hWnd, UINT uMsg, WPARAM wParam, LPARAM lParam) {
     switch (uMsg) {
         case WM_TRAYICON:
             if (lParam == WM_RBUTTONUP) {
@@ -33,14 +50,56 @@ LRESULT CALLBACK WndProc(HWND hWnd, UINT uMsg, WPARAM wParam, LPARAM lParam) {
         case WM_COMMAND:
             if (LOWORD(wParam) == TRAY_EXIT) {
                 PostQuitMessage(0);
+                return 0;
             }
             break;
         case WM_DESTROY:
             PostQuitMessage(0);
-            break;
+            return 0;
+    }
+
+    if (nk_d3d9_handle_event(hWnd, uMsg, wParam, lParam)) {
+        return 0;
     }
 
     return DefWindowProcW(hWnd, uMsg, wParam, lParam);
+}
+
+static HRESULT CreateD3D9Device(HWND hWnd, IDirect3DDevice9Ex** device) {
+    HRESULT hr;
+
+    D3DPRESENT_PARAMETERS params = { 0 };
+    params.PresentationInterval = D3DPRESENT_INTERVAL_DEFAULT;
+    params.BackBufferHeight = WINDOW_WIDTH;
+    params.BackBufferHeight = WINDOW_HEIGHT;
+    params.BackBufferFormat = D3DFMT_X8B8G8R8;
+    params.BackBufferCount = 1;
+    params.MultiSampleType = D3DMULTISAMPLE_NONE;
+    params.SwapEffect = D3DSWAPEFFECT_DISCARD;
+    params.hDeviceWindow = hWnd;
+    params.EnableAutoDepthStencil = TRUE;
+    params.AutoDepthStencilFormat = D3DFMT_D24S8;
+    params.Flags = D3DPRESENTFLAG_DISCARD_DEPTHSTENCIL;
+    params.Windowed = TRUE;
+
+    IDirect3D9Ex* d3d9;
+    hr = Direct3DCreate9Ex(D3D_SDK_VERSION, &d3d9);
+    if (!SUCCEEDED(hr)) {
+        return hr;
+    }
+
+    hr = IDirect3D9Ex_CreateDeviceEx(
+        d3d9,
+        D3DADAPTER_DEFAULT,
+        D3DDEVTYPE_HAL,
+        hWnd,
+        D3DCREATE_HARDWARE_VERTEXPROCESSING | D3DCREATE_PUREDEVICE | D3DCREATE_FPU_PRESERVE,
+        &params,
+        NULL,
+        device);
+
+    IDirect3D9Ex_Release(d3d9);
+    return hr;
 }
 
 int APIENTRY WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, PSTR pCmdLine, int nCmdShow) {
@@ -95,6 +154,7 @@ int APIENTRY WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, PSTR pCmdLine
     wndClass.hInstance = hInstance;
     wndClass.lpszClassName = L"OverlapTrayClass";
 
+    // todo: unregister class
     if (!RegisterClassW(&wndClass)) {
         FreeLibrary(libModule);
         return 1;
@@ -103,17 +163,23 @@ int APIENTRY WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, PSTR pCmdLine
     HWND hWnd = CreateWindowW(
         wndClass.lpszClassName,
         L"Overlap Tray Window",
-        WS_OVERLAPPED,
+        WS_OVERLAPPEDWINDOW | WS_VISIBLE,
         CW_USEDEFAULT,
         CW_USEDEFAULT,
-        0,
-        0,
+        WINDOW_WIDTH,
+        WINDOW_HEIGHT,
         NULL,
         NULL,
         hInstance,
         NULL);
 
     if (!hWnd) {
+        FreeLibrary(libModule);
+        return 1;
+    }
+    IDirect3DDevice9Ex *device;
+    if (!SUCCEEDED(CreateD3D9Device(hWnd, &device))) {
+        DestroyWindow(hWnd);
         FreeLibrary(libModule);
         return 1;
     }
@@ -127,12 +193,16 @@ int APIENTRY WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, PSTR pCmdLine
         LR_SHARED);
 
     if (!icon) {
+        IDirect3DDevice9Ex_Release(device);
+        DestroyWindow(hWnd);
         FreeLibrary(libModule);
         return 1;
     }
 
     HMENU hMenu = CreatePopupMenu();
     if (hMenu == NULL) {
+        IDirect3DDevice9Ex_Release(device);
+        DestroyWindow(hWnd);
         FreeLibrary(libModule);
         return 1;
     }
@@ -154,27 +224,87 @@ int APIENTRY WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, PSTR pCmdLine
 
     if (!Shell_NotifyIconW(NIM_ADD, &data)) {
         DestroyMenu(hMenu);
+        IDirect3DDevice9Ex_Release(device);
+        DestroyWindow(hWnd);
         FreeLibrary(libModule);
         return 1;
     }
 
-    HHOOK hook = SetWindowsHookEx(WH_CBT, (HOOKPROC)proc, libModule, 0);
-    if (!hook) {
-        Shell_NotifyIconW(NIM_DELETE, &data);
+    //HHOOK hook = SetWindowsHookEx(WH_CBT, (HOOKPROC)proc, libModule, 0);
+    //if (!hook) {
+        //Shell_NotifyIconW(NIM_DELETE, &data);
+        //DestroyMenu(hMenu);
+        //FreeLibrary(libModule);
+        //return 1;
+    //}
+
+    // todo: QueryInterface
+    struct nk_context* ctx = nk_d3d9_init((IDirect3DDevice9*)device, WINDOW_WIDTH, WINDOW_HEIGHT);
+
+    if (!ctx) {
         DestroyMenu(hMenu);
+        IDirect3DDevice9Ex_Release(device);
+        DestroyWindow(hWnd);
+        Shell_NotifyIconW(NIM_DELETE, &data);
         FreeLibrary(libModule);
-        return 1;
     }
 
-    MSG msg;
-    while (GetMessageW(&msg, NULL, 0, 0)) {
-        TranslateMessage(&msg);
-        DispatchMessageW(&msg);
+    struct nk_font_atlas *atlas;
+    nk_d3d9_font_stash_begin(&atlas);
+    nk_d3d9_font_stash_end();
+
+    while (1) {
+        MSG msg;
+        HRESULT hr;
+
+        nk_input_begin(ctx);
+        while (GetMessageW(&msg, NULL, 0, 0)) {
+            TranslateMessage(&msg);
+            DispatchMessageW(&msg);
+        }
+        nk_input_end(ctx);
+
+        
+        if (nk_begin(
+            ctx, "Demo",
+            nk_rect(0, 0, WINDOW_WIDTH, WINDOW_HEIGHT),
+            NK_WINDOW_BORDER | NK_WINDOW_TITLE)) {
+
+            nk_layout_row_static(ctx, 30, 80, 1);
+            nk_button_label(ctx, "button");
+
+        }
+        nk_end(ctx);
+
+        // todo: remove these asserts!!!
+        // todo: add like cleanup goto
+        hr = IDirect3DDevice9Ex_Clear(
+            device,
+            0,
+            NULL,
+            D3DCLEAR_TARGET | D3DCLEAR_ZBUFFER | D3DCLEAR_STENCIL,
+            D3DCOLOR_COLORVALUE(0.10f, 0.18f, 0.24f, 1.0f), 0.0f, 0);
+        NK_ASSERT(SUCCEEDED(hr));
+
+        hr = IDirect3DDevice9Ex_BeginScene(device);
+        NK_ASSERT(SUCCEEDED(hr));
+        nk_d3d9_render(NK_ANTI_ALIASING_ON);
+        hr = IDirect3DDevice9Ex_EndScene(device);
+        NK_ASSERT(SUCCEEDED(hr));
+
+        hr = IDirect3DDevice9Ex_PresentEx(device, NULL, NULL, NULL, NULL, 0);
+        if (hr == S_PRESENT_OCCLUDED) {
+            Sleep(10);
+        }
+        NK_ASSERT(SUCCEEDED(hr));
     }
 
-    UnhookWindowsHookEx(hook);
-    Shell_NotifyIconW(NIM_DELETE, &data);
+    //UnhookWindowsHookEx(hook);
+    nk_d3d9_shutdown();
     DestroyMenu(hMenu);
+    IDirect3DDevice9Ex_Release(device);
+    DestroyWindow(hWnd);
+    Shell_NotifyIconW(NIM_DELETE, &data);
     FreeLibrary(libModule);
 
     return 0;
